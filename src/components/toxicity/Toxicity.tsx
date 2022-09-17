4395e8ccd65e553d5bfae1773a1f790e16ebf7ac
Takahiro <tkhr_621@docomo.ne.jp>
2022-09-18 00:37:46 +0900

    入力された文字から測定できるようにした

--- src/components/toxicity/Toxicity.tsx
+++ src/components/toxicity/Toxicity.tsx
@@ -1,32 +1,11 @@
-import { FC, useEffect } from 'react'
-import * as toxicity from '@tensorflow-models/toxicity'
+import { FC } from 'react'
+import { ToxicityForm } from './ToxicityForm'
 
 export const Toxicity: FC = () => {
-  useEffect(() => {
-    const threshold = 0.5
-    const toxicityLabels = [
-      'identity_attack', // 名指しの攻撃
-      'insult', // 侮辱
-      'obscene', // 猥褻
-      'severe_toxicity', // 極端に有害
-      'sexual_explicit', // あからさまに性的な表現
-      'threat', // 脅迫
-      'toxicity', // 有害
-    ]
-
-    // load()はネットワーク経由で実際のモデルファイルをダウンロードする
-    toxicity.load(threshold, toxicityLabels).then((model) => {
-      // 彼女は石器人みたいだ。知性はそれ以下だけど！
-      const sentences = [
-        'She looks like a cavewoman, only far less intelligent!',
-      ]
-
-      model.classify(sentences).then((predictions) => {
-        // console.log(predictions)
-        console.log(JSON.stringify(predictions, null, 2))
-      })
-    })
-  }, [])
-
-  return <h1>Toxicity</h1>
+  return (
+    <div>
+      <h1>Toxicity</h1>
+      <ToxicityForm />
+    </div>
+  )
 }
